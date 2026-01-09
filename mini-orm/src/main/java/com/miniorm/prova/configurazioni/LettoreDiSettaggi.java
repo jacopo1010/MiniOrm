1b72b26a2f3105462357dc855759bc9961b8eedd
jacopo1010 <166029437+jacopo1010@users.noreply.github.com>
2026-01-09 19:00:51 +0100

    aggiunto LettoreDiSettaggi

---
+++ mini-orm/src/main/java/com/miniorm/prova/configurazioni/LettoreDiSettaggi.java
@@ -0,0 +1,9 @@
+package com.miniorm.prova.configurazioni;
+
+import java.util.Properties;
+
+public interface LettoreDiSettaggi {
+     
+	 public Properties carica();
+	 
+}
---
+++ mini-orm/src/main/java/com/miniorm/prova/configurazioni/LettoreDiSettaggiImpl.java
@@ -0,0 +1,34 @@
+package com.miniorm.prova.configurazioni;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Classe che si occupa di individuare file in .xml nel progetto 
+ * con le specifiche per la connessione con il database
+ */
+
+
+public class LettoreDiSettaggiImpl implements LettoreDiSettaggi {
+
+	private final String resourceName = "application.properties";
+
+
+	@Override
+	public Properties carica() {
+		Properties properties = new Properties();
+		try{
+			InputStream in = getClass().getClassLoader().getResourceAsStream(resourceName);
+			if (in == null) {
+				throw new IllegalStateException("File config non trovato: " + resourceName);
+			}
+		    properties.load(in);
+		    return properties;
+		}catch(Exception e) {
+			throw new RuntimeException("Errore lettura config: " + resourceName, e);
+		}
+	}
+
+
+
+}
